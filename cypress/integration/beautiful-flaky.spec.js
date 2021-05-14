64ac4c986791f7f322903635d8f0efeec2a70b31
Jessica Sachs <jess@jessicasachs.io>
2021-05-14 16:29:03 -0400

    moving into it block

--- cypress/integration/beautiful-flaky.spec.js
+++ cypress/integration/beautiful-flaky.spec.js
@@ -1,11 +1,11 @@
-describe('its flaky', {
-  retries: 2
-}, () => {
+describe('its flaky', () => {
   // Cypress._.times(5, (i) => {
   //   it(`T${i + 1}`, () => {
   //     expect(Math.random()).to.be.gt(.5)
   //   })
   // })
 
-  it('is deterministic', () => {})
+  it('is deterministic', {
+  retries: 2
+}, () => {})
 })
