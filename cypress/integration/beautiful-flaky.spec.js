f9cb6f6820ba8cb7dc73c2c8812a28854d5e2827
Jessica Sachs <jess@jessicasachs.io>
2021-05-14 16:29:28 -0400

    Adding retries to a flaky set of tests

--- cypress/integration/beautiful-flaky.spec.js
+++ cypress/integration/beautiful-flaky.spec.js
@@ -1,11 +1,13 @@
-describe('its flaky', () => {
-  // Cypress._.times(5, (i) => {
-  //   it(`T${i + 1}`, () => {
-  //     expect(Math.random()).to.be.gt(.5)
-  //   })
-  // })
+describe('its flaky', {
+  retries: 2
+}, () => {
+  Cypress._.times(5, (i) => {
+    it(`T${i + 1}`, () => {
+      expect(Math.random()).to.be.gt(.5)
+    })
+  })
 
   it('is deterministic', {
-  retries: 2
+  retries: 0
 }, () => {})
 })
